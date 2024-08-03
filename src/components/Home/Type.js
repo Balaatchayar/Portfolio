5fe69f9809c5b55da456d7ce18dcf3fcef6831aa
BALA ATCHAYA R <117425696+Balaatchayar@users.noreply.github.com>
2024-08-03 16:49:21 +0530

    First commit

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,21 +1,9 @@
 import React from "react";
-import Typewriter from "typewriter-effect";
 
 function Type() {
   return (
-    <Typewriter
-      options={{
-        strings: [
-          "Software Developer",
-          "Frontend Developer",
-          "User Interface Designer"
-          
-        ],
-        autoStart: true,
-        loop: true,
-        deleteSpeed: 50,
-      }}
-    />
+    <div>
+    </div>
   );
 }
 
